dbcb8c6198e22ed0e94ded274d789ae9291d01c9
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2017-08-17 23:31:34 +1000

    Loading

--- src/BedgraphFiles.jl
+++ src/BedgraphFiles.jl
@@ -1,5 +1,31 @@
 module BedgraphFiles
 
-# package code goes here
+# using Bedgraph
+using IterableTables, DataValues, DataFrames
+import FileIO
+
+struct BedgraphFile
+    filename::String
+end
+
+function load(f::FileIO.File{FileIO.format"Bedgraph"})
+    return BedgraphFile(f.filename)
+end
+
+IterableTables.isiterable(x::BedgraphFile) = true
+IterableTables.isiterabletable(x::BedgraphFile) = true
+
+function IterableTables.getiterator(file::BedgraphFile)
+
+    # df = Bedgraph.read(file.filename, DataFrame)
+
+    data = readdlm(file.filename)
+
+    df = DataFrame(Chromosome=data[:,1], Start=data[:,2], End=data[:,3], Value=data[:,4])
+
+    it = getiterator(df)
+
+    return it
+end
 
 end # module
