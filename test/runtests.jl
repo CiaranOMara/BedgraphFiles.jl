4802230500d491ff983252ba93c3c8867f75c1c5
Ciarán O’Mara <CiaranOMara@utas.edu.au>
2017-08-28 15:42:10 +1000

    Tweak tests for readability

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,53 @@
 using FileIO
-using IterableTables
-using BedgraphFiles
+using TableTraits
 using DataFrames
 using Base.Test
 
+try add_format(format"Bedgraph", (), [".bedgraph"], [:BedgraphFiles]) end # TODO: Remove once files is registered with FileIO.
+
 @testset "BedgraphFiles" begin
 
-df = load(joinpath(@__DIR__, "data.bedgraph")) |> DataFrame
+const chrom = ["chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19"]
+const chromStart = [49302000, 49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400]
+const chromEnd = [49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400, 49304700]
+const dataValue = [-1.0, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00]
+
+file = joinpath(@__DIR__, "data.bedgraph")
+
+@test isfile(file)
+
+# Load tests.
+loaded = load(file)
+@test isiterable(loaded) == true
+
+## DataFrame
+df = DataFrame(loaded)
 
 @test size(df) == (9,4)
 
-@test df[:chrom] == ["chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19"]
-@test df[:chromStart] == [49302000, 49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400]
-@test df[:chromEnd] == [49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400, 49304700]
-@test df[:dataValue] == [-1.0, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00]
+@test df[:chrom] == chrom
+@test df[:chromStart] == chromStart
+@test df[:chromEnd] == chromEnd
+@test df[:dataValue] == dataValue
 
+## Query DataFrame
+df2 = loaded |> DataFrame
+@test size(df2) == (9,4)
+
+@test df2[:chrom] == chrom
+@test df2[:chromStart] == chromStart
+@test df2[:chromEnd] == chromEnd
+@test df2[:dataValue] == dataValue
+
+# Save test.
 output_filename = tempname() * ".bedgraph"
 
 try
     df |> save(output_filename)
 
-    df2 = load(output_filename) |> DataFrame
+    loaded_df = load(output_filename) |> DataFrame
 
-    @test df == df2
+    @test df == loaded_df
 finally
     gc()
     rm(output_filename)
