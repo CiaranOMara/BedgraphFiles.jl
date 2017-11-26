112aae9c34f2275353d44824a655e447f585614d
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2017-11-26 14:57:36 +1100

    Develop tests
    Try block was removed because it obscured stack trace. Tempory files should eventially be cleared by the operating system. Instead, provide a file path so we can check the written file should an error occur.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,17 @@ using TableTraits
 using DataFrames
 using Base.Test
 
-try add_format(format"Bedgraph", (), [".bedgraph"], [:BedgraphFiles]) end # TODO: Remove once files is registered with FileIO.
+# try add_format(format"Bedgraph", (), [".bedgraph"], [:BedgraphFiles]) end # TODO: Remove once files is registered with FileIO.
 
 @testset "BedgraphFiles" begin
 
-const chrom = ["chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19"]
-const chromStart = [49302000, 49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400]
-const chromEnd = [49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400, 49304700]
-const dataValue = [-1.0, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00]
+const chroms = ["chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19"]
+const chrom_starts = [49302000, 49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400]
+const chrom_ends = [49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400, 49304700]
+const data_values = [-1.0, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00]
 
-file = joinpath(@__DIR__, "data.bedgraph")
+const file = joinpath(@__DIR__, "data.bedgraph")
+const file_headerless = joinpath(@__DIR__, "data-headerless.bedgraph")
 
 @test isfile(file)
 
@@ -25,32 +26,42 @@ df = DataFrame(loaded)
 
 @test size(df) == (9,4)
 
-@test df[:chrom] == chrom
-@test df[:chromStart] == chromStart
-@test df[:chromEnd] == chromEnd
-@test df[:dataValue] == dataValue
+@test df[:chrom] == chroms
+@test df[:chromStart] == chrom_starts
+@test df[:chromEnd] == chrom_ends
+@test df[:dataValue] == data_values
 
 ## Query DataFrame
 df2 = loaded |> DataFrame
 @test size(df2) == (9,4)
 
-@test df2[:chrom] == chrom
-@test df2[:chromStart] == chromStart
-@test df2[:chromEnd] == chromEnd
-@test df2[:dataValue] == dataValue
+@test df2[:chrom] == chroms
+@test df2[:chromStart] == chrom_starts
+@test df2[:chromEnd] == chrom_ends
+@test df2[:dataValue] == data_values
+
+# Load tests.
+loaded_from_headerless = load(file_headerless)
+@test isiterable(loaded_from_headerless) == true
+
+## DataFrame from headerless bedGraph file.
+df_from_headerless = DataFrame(loaded_from_headerless)
+
+@test size(df_from_headerless) == (9,4)
+
+@test df_from_headerless[:chrom] == chroms
+@test df_from_headerless[:chromStart] == chrom_starts
+@test df_from_headerless[:chromEnd] == chrom_ends
+@test df_from_headerless[:dataValue] == data_values
 
 # Save test.
 output_filename = tempname() * ".bedgraph"
+info("Saving to temporary file:", output_filename)
 
-try
-    df |> save(output_filename)
+df |> save(output_filename)
 
-    loaded_df = load(output_filename) |> DataFrame
+loaded_df = load(output_filename) |> DataFrame
 
-    @test df == loaded_df
-finally
-    gc()
-    rm(output_filename)
-end
+@test df == loaded_df
 
 end
