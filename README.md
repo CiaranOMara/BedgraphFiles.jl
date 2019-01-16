1a47b52b970ad20991b87e5617743a74adc870f3
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-01-16 14:17:28 +1100

    Julia v1.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![Project Status: Active - The project has reached a stable, usable state and is being actively developed.](http://www.repostatus.org/badges/latest/active.svg)](http://www.repostatus.org/#active)
 [![Build Status](https://travis-ci.org/CiaranOMara/BedgraphFiles.jl.svg?branch=master)](https://travis-ci.org/CiaranOMara/BedgraphFiles.jl)
 [![Build status](https://ci.appveyor.com/api/projects/status/jny2ep4u3cmly8pj/branch/master?svg=true)](https://ci.appveyor.com/project/CiaranOMara/Bedgraphfiles-jl/branch/master)
-[![BedgraphFiles](http://pkg.julialang.org/badges/BedgraphFiles_0.6.svg)](http://pkg.julialang.org/?pkg=BedgraphFiles)
+[![BedgraphFiles](http://pkg.julialang.org/badges/BedgraphFiles_0.7.svg)](http://pkg.julialang.org/?pkg=BedgraphFiles)
 [![codecov.io](http://codecov.io/github/CiaranOMara/BedgraphFiles.jl/coverage.svg?branch=master)](http://codecov.io/github/CiaranOMara/BedgraphFiles.jl?branch=master)
 [![Coverage Status](https://coveralls.io/repos/github/CiaranOMara/BedgraphFiles.jl/badge.svg?branch=master)](https://coveralls.io/github/CiaranOMara/BedgraphFiles.jl?branch=master)
 
@@ -15,8 +15,12 @@ under the [FileIO.jl](https://github.com/JuliaIO/FileIO.jl) package.
 This package is largely -- if not completely -- inspired by the work of [David Anthoff](https://github.com/davidanthoff).
 
 ## Installation
-
-Use Pkg.add("BedgraphFiles") in Julia to install BedgraphFiles and its dependencies.
+Install BedgraphFiles from the Julia REPL:
+```julia
+using Pkg
+add("BedgraphFiles")
+#Pkg.add("BedgraphFiles") for julia prior to v 0.7
+```
 
 ## Usage
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,17 @@
-using FileIO
+using BedgraphFiles
+using IteratorInterfaceExtensions
 using TableTraits
+using IterableTables
+using FileIO
+using DataValues
 using DataFrames
 using Test
 
 module Bag
 const chroms = ["chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19", "chr19"]
-const chrom_starts = [49302000, 49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400]
-const chrom_ends = [49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400, 49304700]
-const data_values = [-1.0, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00]
+const firsts = [49302000, 49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400]
+const lasts = [49302300, 49302600, 49302900, 49303200, 49303500, 49303800, 49304100, 49304400, 49304700]
+const values = [-1.0, -0.75, -0.50, -0.25, 0.0, 0.25, 0.50, 0.75, 1.00]
 
 const file = joinpath(@__DIR__, "data.bedgraph")
 const file_headerless = joinpath(@__DIR__, "data-headerless.bedgraph")
@@ -19,26 +23,28 @@ end # Bag
 
 # Load tests.
 loaded = load(Bag.file)
-@test isiterable(loaded) == true
+@test IteratorInterfaceExtensions.isiterable(loaded) == true
+@test TableTraits.isiterabletable(loaded) == true
+
 
 ## DataFrame
 df = DataFrame(loaded)
 
 @test size(df) == (9,4)
 
-@test df[:chrom] == chroms
-@test df[:chromStart] == chrom_starts
-@test df[:chromEnd] == chrom_ends
-@test df[:dataValue] == data_values
+@test df[:chrom] == Bag.chroms
+@test df[:first] == Bag.firsts
+@test df[:last] == Bag.lasts
+@test df[:value] == Bag.values
 
 ## Query DataFrame
 df2 = loaded |> DataFrame
 @test size(df2) == (9,4)
 
-@test df2[:chrom] == chroms
-@test df2[:chromStart] == chrom_starts
-@test df2[:chromEnd] == chrom_ends
-@test df2[:dataValue] == data_values
+@test df2[:chrom] == Bag.chroms
+@test df2[:first] == Bag.firsts
+@test df2[:last] == Bag.lasts
+@test df2[:value] == Bag.values
 
 # Load tests.
 loaded_from_headerless = load(Bag.file_headerless)
@@ -49,10 +55,10 @@ df_from_headerless = DataFrame(loaded_from_headerless)
 
 @test size(df_from_headerless) == (9,4)
 
-@test df_from_headerless[:chrom] == chroms
-@test df_from_headerless[:chromStart] == chrom_starts
-@test df_from_headerless[:chromEnd] == chrom_ends
-@test df_from_headerless[:dataValue] == data_values
+@test df_from_headerless[:chrom] == Bag.chroms
+@test df_from_headerless[:first] == Bag.firsts
+@test df_from_headerless[:last] == Bag.lasts
+@test df_from_headerless[:value] == Bag.values
 
 # Save test.
 output_filename = tempname() * ".bedgraph"
