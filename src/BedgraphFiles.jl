e9ec50dfeb5551de91543512f1314766c16a11a7
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-01-31 14:01:40 +1100

    Make load and save methods private

--- src/BedgraphFiles.jl
+++ src/BedgraphFiles.jl
@@ -11,7 +11,6 @@ using TableShowUtils
 
 import IterableTables
 
-export load, save
 
 const BedgraphFileFormat = File{format"bedGraph"}
 
@@ -24,7 +23,7 @@ function Base.show(io::IO, source::BedgraphFile)
     TableShowUtils.printtable(io, getiterator(source), "bedGraph file")
 end
 
-function fileio_load(f::BedgraphFileFormat; args...)
+function load(f::BedgraphFileFormat; args...)
     return BedgraphFile(f.filename, args)
 end
 
@@ -59,20 +58,20 @@ function Base.collect(x::BedgraphFile)
     return collect(getiterator(x))
 end
 
-function fileio_save(file::BedgraphFileFormat, header::Bedgraph.BedgraphHeader, records::Vector{Bedgraph.Record})
+function save(file::BedgraphFileFormat, header::Bedgraph.BedgraphHeader, records::Vector{Bedgraph.Record})
     write(file.filename, header, records)
 end
 
-function fileio_save(file::BedgraphFileFormat, records::Vector{Bedgraph.Record}; bump_forward = true)
+function save(file::BedgraphFileFormat, records::Vector{Bedgraph.Record}; bump_forward = true)
 
     #TODO: bump_forward records.
 
     header = Bedgraph.generateBasicHeader(records, bump_forward = bump_forward)
 
-    return fileio_save(file, header, records)
+    return save(file, header, records)
 end
 
-function fileio_save(file::BedgraphFileFormat, data; bump_forward = true)
+function save(file::BedgraphFileFormat, data; bump_forward = true)
     isiterabletable(data) || error("Can't write this data to bedGraph file.")
 
     it = getiterator(data)
@@ -88,7 +87,7 @@ function fileio_save(file::BedgraphFileFormat, data; bump_forward = true)
 
     header = Bedgraph.generateBasicHeader(records, bump_forward = bump_forward)
 
-    return fileio_save(file, header, records)
+    return save(file, header, records)
 end
 
 end # module
