c62fc18f1c150f430e3111db5ed25a52d51c5d33
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-01-31 14:10:33 +1100

    Convert DataFrameRow to Record

--- src/BedgraphFiles.jl
+++ src/BedgraphFiles.jl
@@ -19,12 +19,16 @@ struct BedgraphFile
     keywords
 end
 
+function Base.convert(::Type{Bedgraph.Record}, row::DataFrameRow) :: Bedgraph.Record
+    return Bedgraph.Record(row[1], row[2], row[3], row[4]) # Note: using index to allow flexible column names.
+end
+
 function Base.convert(::Type{Vector{Bedgraph.Record}}, df::DataFrame) :: Vector{Bedgraph.Record}
 
     records = Vector{Bedgraph.Record}(undef, size(df)[1])
 
     for (i, row) in enumerate(eachrow(df))
-        records[i] =  Bedgraph.Record(row[1], row[2], row[3], row[4]) # Note: using index to allow flexible column names.
+        records[i] = convert(Bedgraph.Record, row)
     end
 
     return records
